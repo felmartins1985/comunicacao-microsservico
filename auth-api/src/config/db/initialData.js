b07fb95f5b53932604420a28144a13f30ba30d4b
Felipe Costa Martins <felmartins1985@gmail.com>
2024-10-08 17:36:55 -0300

    feat: create folder

---
+++ auth-api/src/config/db/initialData.js
@@ -0,0 +1,17 @@
+import bcrypt from 'bcrypt';
+import User from '../../modules/user/model/User.js';
+
+export async function createInitialData() {
+  try{
+    await User.sync({ force: true });
+    let password = bcrypt.hash("123456", 10)
+    await User.create({
+      name: "User test",
+      email: 'testuser@gmail.com',
+      password: password,
+    })
+  }catch(error){
+    console.log("ERRO====>",error);
+  }
+
+}
---
+++ auth-api/src/modules/user/service/UserService.js
@@ -0,0 +1,32 @@
+import userRepository from "../repository/userRepository";
+import * as httpStatus from "../../../config/constants/httpStatus"
+class UserService{
+  async findByEmail(req){
+    try{
+      const {email} = req.params;
+      this.validateRequestData(email);
+      let user = userRepository.findByEmail(email);
+      if(!user){}
+      return {
+        status: httpStatus.SUCCESS,
+        user:{
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        }
+      }
+    } catch (err){
+      return {
+        status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+        message: err.message
+      }
+    }
+  }
+  validateRequestData(email){
+    if(!email){
+      throw new Error("Email é obrigatório");
+    }
+  }
+}
+
+export default new UserService();
